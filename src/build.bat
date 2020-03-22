52df734ce9bb8e53beebf46b0efb0ff03546e261
lovac42 <lovac42@protonmail.com>
2020-03-22 03:18:20 -0400

    version bump v0.2.0

--- src/build.bat
+++ src/build.bat
@@ -3,7 +3,7 @@ set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=DancingBaloney
 set NAME=Dancing Baloney
 set PACKID=378638814
-set VERSION=0.1.0
+set VERSION=0.2.0
 
 
 quick_manifest.exe "%NAME%" "%PACKID%" >%REPO%\manifest.json
