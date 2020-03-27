b7e162d632ec5977ca8d5dc38a88316907c13794
lovac42 <lovac42@protonmail.com>
2020-03-27 01:20:28 -0400

    version bump v0.4.1

--- src/build.bat
+++ src/build.bat
@@ -3,10 +3,8 @@ set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=DancingBaloney
 set NAME=Dancing Baloney
 set PACKID=378638814
-set VERSION=0.4.0
+set VERSION=0.4.1
 
-
-quick_manifest.exe "%NAME%" "%PACKID%" >%REPO%\manifest.json
 echo %VERSION% >%REPO%\VERSION
 
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
@@ -14,5 +12,8 @@ move checksum.md5 %REPO%\checksum.md5
 
 REM %ZIP% %REPO%_v%VERSION%_Anki20.zip *.py %REPO%\*
 cd %REPO%
+quick_manifest.exe "%NAME%" "%PACKID%" >manifest.json
 %ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
+
+quick_manifest.exe "%NAME%" "%NAME%" >manifest.json
 %ZIP% ../%REPO%_v%VERSION%_CCBC.adze *
